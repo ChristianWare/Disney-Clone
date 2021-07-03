82a66c7a3cc43a800af3699c88b0a9eb43faea40
Chris <chris.ware.dev@gmail.com>
2021-07-02 20:28:03 -0700

    ADD: Grab Movies from DB

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
+import db from "../firebase"
+import { useDispatch } from "react-redux";
+import { setMovies } from '../features/movie/movieSlice'
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  db.collection("movies").onSnapshot((snapshot) => {
+    let tempMovies = snapshot.docs.map((doc)=> {
+        return { id: doc.id, ...doc.data() }
+    })
+    dispatch(setMovies(tempMovies));
+  });
+}, []);
+
   return (
     <Container>
       <ImgSlider />
@@ -14,7 +28,7 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home; 
 
 const Container = styled.main`
   min-height: calc(100vh - 70px);
